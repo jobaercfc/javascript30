b449c54e69789e40c9f47de5cb15e3a203aec371
jobaercfc <surid.cfc95@gmail.com>
2021-09-25 07:26:32 +0900

    [Update] index.php updated

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
     <head></head>
     <body>
-        <ul>
-            <li><a href="JS-Drum-kit/index.html">1. JS drums</a></li>
-            <li><a href="JS-CSS-Clock/index.html">2. JS CSS Analog Clock</a></li>
-            <li><a href="Slider/index.html">3. Slider</a></li>
-        </ul>
+        <ol>
+            <li><a href="JS-Drum-kit/index.html">JS drums</a></li>
+            <li><a href="JS-CSS-Clock/index.html">JS CSS Analog Clock</a></li>
+            <li><a href="Slider/index.html">Slider</a></li>
+        </ol>
     </body>
 </html>
